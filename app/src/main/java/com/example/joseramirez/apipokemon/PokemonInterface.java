86d683d290fdab45244c763d36c4cfb9c8ed04b0
joseramirez <e.josem91@gmail.com>
2018-05-12 22:14:48 -0600

    I add method at interface

--- app/src/main/java/com/example/joseramirez/apipokemon/PokemonInterface.java
+++ app/src/main/java/com/example/joseramirez/apipokemon/PokemonInterface.java
@@ -1,11 +1,16 @@
 package com.example.joseramirez.apipokemon;
 
+import com.example.joseramirez.apipokemon.Model.Generation;
 import com.example.joseramirez.apipokemon.Model.Region;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface PokemonInterface {
     @GET("region")
     Call<Region> getRegions();
+    @GET("generation/{id}")
+    Call<Generation> getGeneration( @Path("id") String id );
+
 }
